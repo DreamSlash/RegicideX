747b5694dad6be7803c040bfcd6733971d525d30
= <alvar.alonso.2010@gmail.com>
2022-04-25 15:11:46 +0200

    execute_damage execution. generic damage gameplay effect.

---
+++ Source/RegicideX/GAS/AbilityTasks/RGX_AT_PlayMontageAndWaitForEvent.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "RGX_AT_PlayMontageAndWaitForEvent.generated.h"
+
+// TO BE IMPLEMENTED
---
+++ Source/RegicideX/GAS/Executions/RGX_Execution_Damage.cpp
@@ -0,0 +1,55 @@
+#include "RGX_Execution_Damage.h"
+#include "../AttributeSets/RGX_HealthAttributeSet.h"
+#include "../AttributeSets/RGX_CombatAttributeSet.h"
+
+struct RGX_DamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Health)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DamageMitigation)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower)
+
+	RGX_DamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(URGX_HealthAttributeSet, Health, Target, true);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(URGX_CombatAttributeSet, DamageMitigation, Target, true);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(URGX_CombatAttributeSet, AttackPower, Source, true);
+	}
+};
+
+static const RGX_DamageStatics& DamageStatics()
+{
+	static RGX_DamageStatics damageStatics;
+	return damageStatics;
+}
+
+UExecution_Damage::UExecution_Damage()
+{
+	RelevantAttributesToCapture.Add(DamageStatics().HealthDef);
+	RelevantAttributesToCapture.Add(DamageStatics().DamageMitigationDef);
+	RelevantAttributesToCapture.Add(DamageStatics().AttackPowerDef);
+}
+
+void UExecution_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float DamageMitigation = 0.0f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().DamageMitigationDef, EvaluationParameters, DamageMitigation);
+
+	float AttackPower = 0.0f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().AttackPowerDef, EvaluationParameters, AttackPower);
+
+	float FinalDamage = 0.0f;
+	FinalDamage = AttackPower * FMath::Max(0.0f, (1.0f - DamageMitigation));
+
+	FGameplayEventData EventPayload;
+	ExecutionParams.GetTargetAbilitySystemComponent()->HandleGameplayEvent(FGameplayTag::RequestGameplayTag("GameplayEvent.Combat.TakeDamage"), &EventPayload);
+
+	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(RGX_DamageStatics().HealthProperty, EGameplayModOp::Additive, -FinalDamage));
+}
