b216057c633d184308a304e41cd62f2e34f8da84
PabloAcedo <pabloa22.98@gmail.com>
2022-05-21 20:57:25 +0200

    Melee Angel Birth

---
+++ Source/RegicideX/Actors/Enemies/RGX_MeleeAngel.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RGX_MeleeAngel.h"
+
+ARGX_MeleeAngel::ARGX_MeleeAngel()
+{
+
+}
---
+++ Source/RegicideX/Actors/Enemies/RGX_MeleeAngel.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RGX_EnemyBase.h"
+#include "RGX_MeleeAngel.generated.h"
+
+/**
+ * 
+ */
+
+
+UCLASS()
+class REGICIDEX_API ARGX_MeleeAngel : public ARGX_EnemyBase
+{
+	GENERATED_BODY()
+	
+public:
+	ARGX_MeleeAngel();
+};
