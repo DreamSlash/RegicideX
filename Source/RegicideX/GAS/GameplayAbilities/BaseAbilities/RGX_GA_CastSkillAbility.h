5de0170834307c51cc07df32698e09719e26f323
= <alvar.alonso.2010@gmail.com>
2022-05-24 18:39:09 +0200

    Barrier skill refactor.

--- Source/RegicideX/GAS/GameplayAbilities/BaseAbilities/RGX_GA_CastSkillAbility.h
+++ Source/RegicideX/GAS/GameplayAbilities/BaseAbilities/RGX_GA_CastSkillAbility.h
@@ -18,7 +18,7 @@ public:
 	URGX_CastSkillAbility();
 
 protected:
-	bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const;
+	bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
 
 	void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
--- Source/RegicideX/GAS/GameplayAbilities/Player/RGX_GA_BarrierAbility.cpp
+++ Source/RegicideX/GAS/GameplayAbilities/Player/RGX_GA_BarrierAbility.cpp
@@ -1,44 +1,36 @@
 #include "RGX_GA_BarrierAbility.h"
 #include "GameFramework/Character.h"
 #include "Kismet/KismetSystemLibrary.h"
-#include "../../../Actors/Enemies/RGX_EnemyBase.h"
+#include "RegicideX/Actors/Enemies/RGX_EnemyBase.h"
 #include "Components/MCV_AbilitySystemComponent.h"
 #include "UObject/UObjectGlobals.h"
-#include "../../../Character/RGX_PlayerCharacter.h"
+#include "RegicideX/Character/RGX_PlayerCharacter.h"
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+
+URGX_BarrierAbility::URGX_BarrierAbility()
+{
+
+}
+
+bool URGX_BarrierAbility::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
+{
+	bool bResult = Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+	if (bResult == false)
+		return false;
+
+	ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor);
+	if (Character == nullptr)
+		return false;
+
+	return true;
+}
 
 void URGX_BarrierAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
-	// TODO: Ensure size of tag and event arrays are the same.
-
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-
-	if (CommitAbility(Handle, ActorInfo, ActivationInfo) == false)
-		return;
-
 	FGameplayEventData EventData;
 	CurrentActorInfo->AbilitySystemComponent->HandleGameplayEvent(FGameplayTag::RequestGameplayTag(FName("GameplayEvent.Character.Interrupted")), &EventData);
 
-	ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor);
-	URGX_CombatAssistComponent* CombatAssistComponent = Character->FindComponentByClass<URGX_CombatAssistComponent>();
-
-	CombatAssistComponent->PerformAttackAutoAssist();
-
-	if (Character)
-	{
-		UAbilityTask_PlayMontageAndWait* PlayMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, MontageToPlay);
-		PlayMontageTask->OnBlendOut.AddDynamic(this, &URGX_BarrierAbility::FinishBarrierSkill);
-		PlayMontageTask->OnCancelled.AddDynamic(this, &URGX_BarrierAbility::FinishBarrierSkill);
-		PlayMontageTask->OnCompleted.AddDynamic(this, &URGX_BarrierAbility::FinishBarrierSkill);
-		PlayMontageTask->OnInterrupted.AddDynamic(this, &URGX_BarrierAbility::FinishBarrierSkill);
-		PlayMontageTask->ReadyForActivation();
-	}
-	else
-	{
-		EndAbility(Handle, ActorInfo, ActivationInfo, false, false);
-	}
-
-	CastBarrierSkill(ActorInfo->AvatarActor.Get());
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
 
 void URGX_BarrierAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
@@ -46,14 +38,12 @@ void URGX_BarrierAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, co
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
 
-void URGX_BarrierAbility::CastBarrierSkill(AActor* CasterActor)
+void URGX_BarrierAbility::OnReceivedEvent(FGameplayTag EventTag, FGameplayEventData EventData)
 {
-	FVector SpawnLocation = CasterActor->GetActorLocation();
+	FVector SpawnLocation = CurrentActorInfo->AvatarActor->GetActorLocation();
 	FRotator SpawnRotation = FRotator(0.0f);
 
 	AActor* SpawnedBarrier = GetWorld()->SpawnActor<AActor>(BarrierClass, SpawnLocation, SpawnRotation);
-
-	float radius = 200.0f;
 
 	TArray<TEnumAsByte<EObjectTypeQuery>> TraceObjectTypes;
 	TraceObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn));
@@ -63,12 +53,12 @@ void URGX_BarrierAbility::CastBarrierSkill(AActor* CasterActor)
 	TArray<AActor*> IgnoreActors;
 	TArray<AActor*> OutActors;
 
-	UKismetSystemLibrary::SphereOverlapActors(GetWorld(), SpawnLocation, radius, TraceObjectTypes, SeekClass, IgnoreActors, OutActors);
+	UKismetSystemLibrary::SphereOverlapActors(GetWorld(), SpawnLocation, Radius, TraceObjectTypes, SeekClass, IgnoreActors, OutActors);
 
 	for (AActor* Actor : OutActors)
 	{
 		ARGX_EnemyBase* Enemy = Cast<ARGX_EnemyBase>(Actor);
-		
+
 		FVector EnemyLocation = Enemy->GetActorLocation();
 		FVector DirectionVector = EnemyLocation - SpawnLocation;
 		DirectionVector.Normalize();
@@ -83,14 +73,9 @@ void URGX_BarrierAbility::CastBarrierSkill(AActor* CasterActor)
 		UKismetSystemLibrary::DrawDebugArrow(GetWorld(), LineStart, LineEnd, 10.0f, ArrowColor, 3.0f, 2.0f);
 
 		FGameplayEventData EventData;
-		EventData.Instigator = CasterActor;
+		EventData.Instigator = CurrentActorInfo->AvatarActor.Get();
 		EventData.OptionalObject = LaunchEventData;
 
 		Enemy->GetAbilitySystemComponent()->HandleGameplayEvent(FGameplayTag::RequestGameplayTag(FName("GameplayEvent.Launched")), &EventData);
 	}
-}
-
-void URGX_BarrierAbility::FinishBarrierSkill()
-{
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false, false);
 }
