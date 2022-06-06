1e26817f285c1f33ac333572131b6d8a05a5658f
= <alvar.alonso.2010@gmail.com>
2022-06-06 14:57:47 +0200

    AN_SendEventToSelf to c++.

--- Source/RegicideX/Notifies/RGX_AN_SendEventToSelf.cpp
+++ Source/RegicideX/Notifies/RGX_AN_SendEventToSelf.cpp
@@ -2,4 +2,20 @@
 
 
 #include "RegicideX/Notifies/RGX_AN_SendEventToSelf.h"
+#include "AbilitySystemComponent.h"
+#include "RegicideX/GAS/RGX_PayloadObjects.h"
 
+void URGX_AN_SendEventToSelf::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	AActor* Owner = MeshComp->GetOwner();
+
+	UAbilitySystemComponent* ACS = Owner->FindComponentByClass<UAbilitySystemComponent>();
+	if (ACS)
+	{
+		FGameplayEventData EventData;
+		EventData.EventTag = EventTag;
+		EventData.Instigator = Owner;
+		EventData.OptionalObject = EventPayload;
+		ACS->HandleGameplayEvent(EventTag, &EventData);
+	}
+}
--- Source/RegicideX/Notifies/RGX_AN_SendEventToSelf.h
+++ Source/RegicideX/Notifies/RGX_AN_SendEventToSelf.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimNotifies/AnimNotify.h"
+#include "GameplayTags.h"
 #include "RGX_AN_SendEventToSelf.generated.h"
 
+class URGX_RGXEventDataAsset;
 /**
  * 
  */
@@ -14,4 +16,12 @@ class REGICIDEX_API URGX_AN_SendEventToSelf : public UAnimNotify
 {
 	GENERATED_BODY()
 	
+	void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayTag EventTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	URGX_RGXEventDataAsset* EventPayload;
 };
