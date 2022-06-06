43940dad6ba3eec3c9381e76abcb25d1b4b9683c
= <alvar.alonso.2010@gmail.com>
2022-06-06 14:25:35 +0200

    ANS_ActivateHitboxEffect to c++.

--- Source/RegicideX/Notifies/RGX_ANS_ActivateHitboxEffect.cpp
+++ Source/RegicideX/Notifies/RGX_ANS_ActivateHitboxEffect.cpp
@@ -2,4 +2,29 @@
 
 
 #include "RegicideX/Notifies/RGX_ANS_ActivateHitboxEffect.h"
+#include "RegicideX/Components/RGX_HitboxesManagerComponent.h"
+#include "RegicideX/Components/RGX_HitboxComponent.h"
 
+void URGX_ANS_ActivateHitboxEffect::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration)
+{
+	AActor* Owner = MeshComp->GetOwner();
+
+	URGX_HitboxesManagerComponent* HitboxesManagerComponent = Owner->FindComponentByClass<URGX_HitboxesManagerComponent>();
+	if (HitboxesManagerComponent)
+	{
+		URGX_HitboxComponent* Hitbox = HitboxesManagerComponent->GetHitboxByTag(HitboxTag);
+		Hitbox->ActivateEffect();
+	}
+}
+
+void URGX_ANS_ActivateHitboxEffect::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	AActor* Owner = MeshComp->GetOwner();
+
+	URGX_HitboxesManagerComponent* HitboxesManagerComponent = Owner->FindComponentByClass<URGX_HitboxesManagerComponent>();
+	if (HitboxesManagerComponent)
+	{
+		URGX_HitboxComponent* Hitbox = HitboxesManagerComponent->GetHitboxByTag(HitboxTag);
+		Hitbox->DeactivateEffect();
+	}
+}
--- Source/RegicideX/Notifies/RGX_ANS_ActivateHitboxEffect.h
+++ Source/RegicideX/Notifies/RGX_ANS_ActivateHitboxEffect.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "GameplayTags.h"
 #include "RGX_ANS_ActivateHitboxEffect.generated.h"
 
 /**
@@ -14,4 +15,10 @@ class REGICIDEX_API URGX_ANS_ActivateHitboxEffect : public UAnimNotifyState
 {
 	GENERATED_BODY()
 	
+	void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration) override;
+	void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayTag HitboxTag;
 };
