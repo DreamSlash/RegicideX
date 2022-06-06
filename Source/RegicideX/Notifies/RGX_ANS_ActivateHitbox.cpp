c0bacff24fb88b51f4f3129c0dcd474dee2e6fdb
= <alvar.alonso.2010@gmail.com>
2022-06-06 14:38:10 +0200

    ANS_ActivateHitbox to c++.

--- Source/RegicideX/Notifies/RGX_ANS_ActivateHitbox.cpp
+++ Source/RegicideX/Notifies/RGX_ANS_ActivateHitbox.cpp
@@ -2,4 +2,29 @@
 
 
 #include "RegicideX/Notifies/RGX_ANS_ActivateHitbox.h"
+#include "RegicideX/Components/RGX_HitboxesManagerComponent.h"
+#include "RegicideX/Components/RGX_HitboxComponent.h"
 
+void URGX_ANS_ActivateHitbox::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration)
+{
+	AActor* Owner = MeshComp->GetOwner();
+
+	URGX_HitboxesManagerComponent* HitboxesManagerComponent = Owner->FindComponentByClass<URGX_HitboxesManagerComponent>();
+	if (HitboxesManagerComponent)
+	{
+		URGX_HitboxComponent* Hitbox = HitboxesManagerComponent->GetHitboxByTag(HitboxTag);
+		Hitbox->ActivateHitbox();
+	}
+}
+
+void URGX_ANS_ActivateHitbox::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	AActor* Owner = MeshComp->GetOwner();
+
+	URGX_HitboxesManagerComponent* HitboxesManagerComponent = Owner->FindComponentByClass<URGX_HitboxesManagerComponent>();
+	if (HitboxesManagerComponent)
+	{
+		URGX_HitboxComponent* Hitbox = HitboxesManagerComponent->GetHitboxByTag(HitboxTag);
+		Hitbox->DeactivateHitbox();
+	}
+}
