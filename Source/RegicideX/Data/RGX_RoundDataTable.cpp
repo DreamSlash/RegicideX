934bf486a62395a8502ff2fe03c430aad9a36ba6
Ricard Hidalgo <ricard.hidalgo.95@gmail.com>
2022-06-11 20:06:55 +0200

    Fix Rounds

--- Source/RegicideX/Data/RGX_RoundDataTable.cpp
+++ Source/RegicideX/Data/RGX_RoundDataTable.cpp
@@ -2,11 +2,5 @@
 
 FRGX_RoundDataTable::FRGX_RoundDataTable()
 {
-	EnemiesToSpawn.Init(0,4);
-}
-
-void FRGX_RoundDataTable::OnDataTableChanged(const UDataTable* InDataTable, const FName InRowName)
-{
-	
-	FTableRowBase::OnDataTableChanged(InDataTable, InRowName);
+	EnemiesToSpawn.Init(0,5);
 }
