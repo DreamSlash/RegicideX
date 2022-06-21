4ed9671ba036aa6def4e7b899471ea516c71bbb2
= <alvar.alonso.2010@gmail.com>
2022-06-21 20:43:44 +0200

    AT_FollowActor basic interface

--- Source/RegicideX/GAS/AbilityTasks/RGX_AT_FollowActor.cpp
+++ Source/RegicideX/GAS/AbilityTasks/RGX_AT_FollowActor.cpp
@@ -3,3 +3,28 @@
 
 #include "RegicideX/GAS/AbilityTasks/RGX_AT_FollowActor.h"
 
+void URGX_AT_FollowActor::InitSimulatedTask(UGameplayTasksComponent& InGameplayTasksComponent)
+{
+	Super::InitSimulatedTask(InGameplayTasksComponent);
+}
+
+URGX_AT_FollowActor* URGX_AT_FollowActor::FollowActor(UGameplayAbility* OwningAbility, FName TaskInstanceName, float Speed)
+{
+	URGX_AT_FollowActor* MyObj = NewAbilityTask<URGX_AT_FollowActor>(OwningAbility, TaskInstanceName);
+
+	return MyObj;
+}
+
+void URGX_AT_FollowActor::Activate()
+{
+}
+
+void URGX_AT_FollowActor::TickTask(float DeltaTime)
+{
+	Super::TickTask(DeltaTime);
+}
+
+void URGX_AT_FollowActor::OnDestroy(bool AbilityIsEnding)
+{
+	Super::OnDestroy(AbilityIsEnding);
+}
--- Source/RegicideX/GAS/AbilityTasks/RGX_AT_FollowActor.h
+++ Source/RegicideX/GAS/AbilityTasks/RGX_AT_FollowActor.h
@@ -6,6 +6,8 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "RGX_AT_FollowActor.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FFollowActorDelegate);
+
 /**
  * 
  */
@@ -14,4 +16,19 @@ class REGICIDEX_API URGX_AT_FollowActor : public UAbilityTask
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(BlueprintAssignable)
+	FFollowActorDelegate		OnTargetLocationReached;
+
+	void InitSimulatedTask(UGameplayTasksComponent& InGameplayTasksComponent) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "TRUE"))
+	static URGX_AT_FollowActor* FollowActor(UGameplayAbility* OwningAbility, FName TaskInstanceName, float Speed);
+
+	void Activate() override;
+
+	/** Tick function for this task, if bTickingTask == true */
+	void TickTask(float DeltaTime) override;
+
+	void OnDestroy(bool AbilityIsEnding) override;
 };
