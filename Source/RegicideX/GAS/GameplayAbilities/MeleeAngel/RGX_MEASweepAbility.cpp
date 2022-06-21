e6746cec8cbc52661cb160c75d450949650ea943
= <alvar.alonso.2010@gmail.com>
2022-06-21 20:07:14 +0200

    MEAngel Sweep Ability created

---
+++ Source/RegicideX/GAS/GameplayAbilities/MeleeAngel/RGX_MEASweepAbility.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RegicideX/GAS/GameplayAbilities/MeleeAngel/RGX_MEASweepAbility.h"
+
+bool URGX_MEASweepAbility::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const 
+{
+	bool bResult = Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+	if (bResult == false)
+		return false;
+
+	return true;
+}
+
+
+void URGX_MEASweepAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+
+}
+
+void URGX_MEASweepAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+
+}
