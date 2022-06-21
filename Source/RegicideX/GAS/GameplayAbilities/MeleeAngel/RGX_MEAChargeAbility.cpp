889a39773d4c9f24f8635b11f9273b379c6c79db
= <alvar.alonso.2010@gmail.com>
2022-06-21 17:08:43 +0200

    melee angel charges to correct point

--- Source/RegicideX/GAS/GameplayAbilities/MeleeAngel/RGX_MEAChargeAbility.cpp
+++ Source/RegicideX/GAS/GameplayAbilities/MeleeAngel/RGX_MEAChargeAbility.cpp
@@ -6,6 +6,9 @@
 #include "RegicideX/GAS/AbilityTasks/RGX_MoveToLocationCharge.h"
 #include "Abilities/Tasks/AbilityTask_MoveToLocation.h"
 #include "RegicideX/Actors/Enemies/RGX_EnemyBase.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "RegicideX/Actors/Enemies/RGX_MeleeAngel.h"
 
 bool URGX_MEAChargeAbility::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
 {
@@ -28,7 +31,37 @@ void URGX_MEAChargeAbility::ActivateAbility(const FGameplayAbilitySpecHandle Han
 
 	// Execute move task
 	ARGX_EnemyBase* Enemy = Cast<ARGX_EnemyBase>(GetAvatarActorFromActorInfo());
+	ARGX_MeleeAngel* MeleeAngel = Cast<ARGX_MeleeAngel>(Enemy);
+	if (MeleeAngel)
+	{
+		MeleeAngel->bFlying = true;
+	}
+
+	const FVector CurrentLocation = Enemy->GetActorLocation();
 	FVector TargetLocation = Enemy->TargetActor->GetActorLocation();
+
+	const FRotator RotationOffset = UKismetMathLibrary::FindLookAtRotation(CurrentLocation, TargetLocation);
+	Enemy->SetActorRotation(RotationOffset);
+
+	const FVector EnemyForward = Enemy->GetActorForwardVector();
+	const FVector RaySrc = CurrentLocation + EnemyForward * 50.0f;
+	const FVector RayEnd = CurrentLocation + EnemyForward * 10000.0f;
+	FHitResult RayTraceResult;
+
+	TargetLocation = CurrentLocation + EnemyForward * 10000.0f;
+
+	if (GetWorld()->LineTraceSingleByChannel(RayTraceResult, RaySrc, RayEnd, ECollisionChannel::ECC_WorldStatic))
+	{
+		if (RayTraceResult.ImpactPoint != FVector(0.0, 0.0, 0.0))
+		{
+			TargetLocation = RayTraceResult.ImpactPoint;
+		}
+	}
+
+	UKismetSystemLibrary::DrawDebugLine(GetWorld(), RaySrc, RayEnd, FColor(255, 0, 0), 5.0, 5.0f);
+	UKismetSystemLibrary::DrawDebugPoint(GetWorld(), TargetLocation, 100.0f, FColor(255, 255, 255), 5.0f);
+
+	// TODO: Change this task for a task that follows the player until collision
 	URGX_MoveToLocationCharge* MoveToLocationTask = URGX_MoveToLocationCharge::MoveToLocationCharge(this, NAME_None, TargetLocation, 5.0f, nullptr, nullptr);
 	MoveToLocationTask->OnTargetLocationReached.AddDynamic(this, &URGX_MEAChargeAbility::OnDestinationReached);
 	//UAbilityTask_MoveToLocation* MoveToLocationTask = UAbilityTask_MoveToLocation::MoveToLocation(this, NAME_None, TargetLocation, 5.0f, nullptr, nullptr);
