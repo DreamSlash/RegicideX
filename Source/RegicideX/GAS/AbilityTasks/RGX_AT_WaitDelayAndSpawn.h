07a73328026773c738b377009d99676e1d506584
= <alvar.alonso.2010@gmail.com>
2022-06-28 13:28:12 +0200

    WaitDelayAndSpawn AbilityTask Created

---
+++ Source/RegicideX/GAS/AbilityTasks/RGX_AT_WaitDelayAndSpawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask_WaitDelay.h"
+#include "RGX_AT_WaitDelayAndSpawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REGICIDEX_API URGX_AT_WaitDelayAndSpawn : public UAbilityTask_WaitDelay
+{
+	GENERATED_BODY()
+	
+};
--- Source/RegicideX/GAS/GameplayAbilities/DistanceAngel/RGX_GA_BulletHell.cpp
+++ Source/RegicideX/GAS/GameplayAbilities/DistanceAngel/RGX_GA_BulletHell.cpp
@@ -2,6 +2,8 @@
 
 
 #include "RegicideX/GAS/GameplayAbilities/DistanceAngel/RGX_GA_BulletHell.h"
+#include "Components/SceneComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 bool URGX_GA_BulletHell::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
 {
@@ -10,10 +12,24 @@ bool URGX_GA_BulletHell::CanActivateAbility(const FGameplayAbilitySpecHandle Han
 
 void URGX_GA_BulletHell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
+	UE_LOG(LogTemp, Warning, TEXT("Bullet Hell\n"));
+
+	TArray<UActorComponent*> BulletOrigins = GetAvatarActorFromActorInfo()->GetComponentsByTag(USceneComponent::StaticClass(), FName("BulletHell"));
+
+	for (const UActorComponent* ActorOrigin : BulletOrigins)
+	{
+		const USceneComponent* SceneOrigin = Cast<USceneComponent>(ActorOrigin);
+		const FVector BulletOrigin = SceneOrigin->GetComponentLocation();
+		const FRotator BulletRotation = SceneOrigin->GetComponentRotation();
+
+		const FVector BulletDestination = BulletOrigin + SceneOrigin->GetForwardVector() * 1000.0f;
+		UKismetSystemLibrary::DrawDebugLine(GetWorld(), BulletOrigin, BulletDestination, FColor(255, 0, 0), 5.0, 5.0f);
+	}
 }
 
 void URGX_GA_BulletHell::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
-
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
