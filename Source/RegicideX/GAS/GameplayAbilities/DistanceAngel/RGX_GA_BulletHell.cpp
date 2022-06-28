4164ac175f43007fadac4d0073d381cea97a7372
= <alvar.alonso.2010@gmail.com>
2022-06-28 12:10:09 +0200

    GA_BulletHell CanActivateAbility fix

--- Source/RegicideX/GAS/GameplayAbilities/DistanceAngel/RGX_GA_BulletHell.cpp
+++ Source/RegicideX/GAS/GameplayAbilities/DistanceAngel/RGX_GA_BulletHell.cpp
@@ -5,7 +5,7 @@
 
 bool URGX_GA_BulletHell::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
 {
-
+	return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
 }
 
 void URGX_GA_BulletHell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
