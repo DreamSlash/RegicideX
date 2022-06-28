b7a93dac4a2b6423eb684800d604f20b2d989fb1
= <alvar.alonso.2010@gmail.com>
2022-06-28 13:58:47 +0200

    WaitDelayAndSpawn task basic interface

--- Source/RegicideX/GAS/AbilityTasks/RGX_AT_WaitDelayAndSpawn.cpp
+++ Source/RegicideX/GAS/AbilityTasks/RGX_AT_WaitDelayAndSpawn.cpp
@@ -2,4 +2,59 @@
 
 
 #include "RegicideX/GAS/AbilityTasks/RGX_AT_WaitDelayAndSpawn.h"
+#include "AbilitySystemGlobals.h"
+#include "TimerManager.h"
 
+URGX_AT_WaitDelayAndSpawn::URGX_AT_WaitDelayAndSpawn(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bTickingTask = true;
+	Time = 0.f;
+	TimeStarted = 0.f;
+}
+
+URGX_AT_WaitDelayAndSpawn* URGX_AT_WaitDelayAndSpawn::WaitDelayAndSpawn(UGameplayAbility* OwningAbility, float Time)
+{
+	UAbilitySystemGlobals::NonShipping_ApplyGlobalAbilityScaler_Duration(Time);
+
+	URGX_AT_WaitDelayAndSpawn* MyObj = NewAbilityTask<URGX_AT_WaitDelayAndSpawn>(OwningAbility);
+	MyObj->Time = Time;
+	return MyObj;
+}
+
+void URGX_AT_WaitDelayAndSpawn::Activate()
+{
+	UWorld* World = GetWorld();
+	TimeStarted = World->GetTimeSeconds();
+
+	// Use a dummy timer handle as we don't need to store it for later but we don't need to look for something to clear
+	FTimerHandle TimerHandle;
+	World->GetTimerManager().SetTimer(TimerHandle, this, &URGX_AT_WaitDelayAndSpawn::OnTimeFinish, Time, false);
+}
+
+FString URGX_AT_WaitDelayAndSpawn::GetDebugString() const
+{
+	float TimeLeft = Time - GetWorld()->TimeSince(TimeStarted);
+	return FString::Printf(TEXT("WaitDelay. Time: %.2f. TimeLeft: %.2f"), Time, TimeLeft);
+}
+
+void URGX_AT_WaitDelayAndSpawn::TickTask(float DeltaTime)
+{
+	Super::TickTask(DeltaTime);
+
+	UE_LOG(LogTemp, Warning, TEXT("Bullet Hell Tick\n"));
+}
+
+void URGX_AT_WaitDelayAndSpawn::OnDestroy(bool AbilityIsEnding)
+{
+	Super::OnDestroy(AbilityIsEnding);
+}
+
+void URGX_AT_WaitDelayAndSpawn::OnTimeFinish()
+{
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		OnFinish.Broadcast();
+	}
+	EndTask();
+}
--- Source/RegicideX/GAS/AbilityTasks/RGX_AT_WaitDelayAndSpawn.h
+++ Source/RegicideX/GAS/AbilityTasks/RGX_AT_WaitDelayAndSpawn.h
@@ -3,15 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Abilities/Tasks/AbilityTask_WaitDelay.h"
+#include "UObject/ObjectMacros.h"
+#include "Abilities/Tasks/AbilityTask.h"
 #include "RGX_AT_WaitDelayAndSpawn.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWaitDelayDelegate);
 /**
  * 
  */
 UCLASS()
-class REGICIDEX_API URGX_AT_WaitDelayAndSpawn : public UAbilityTask_WaitDelay
+class REGICIDEX_API URGX_AT_WaitDelayAndSpawn : public UAbilityTask
 {
-	GENERATED_BODY()
-	
+	GENERATED_UCLASS_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FWaitDelayDelegate	OnFinish;
+
+	virtual void Activate() override;
+
+	/** Return debug string describing task */
+	virtual FString GetDebugString() const override;
+
+	/** Wait specified time. This is functionally the same as a standard Delay node. Spawn specified actors at a specified rate */
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "TRUE"))
+	static URGX_AT_WaitDelayAndSpawn* WaitDelayAndSpawn(UGameplayAbility* OwningAbility, float Time);
+
+	void TickTask(float DeltaTime) override;
+	void OnDestroy(bool AbilityIsEnding) override;
+
+private:
+	void OnTimeFinish();
+
+	float Time;
+	float TimeStarted;
 };
