255d7030dd0b84ab0874494d10aee19e7350ea63
= <alvar.alonso.2010@gmail.com>
2022-07-01 13:32:48 +0200

    RGX_Projectile basic interface

--- Source/RegicideX/Actors/Projectiles/RGX_Projectile.cpp
+++ Source/RegicideX/Actors/Projectiles/RGX_Projectile.cpp
@@ -2,6 +2,7 @@
 
 
 #include "RegicideX/Actors/Projectiles/RGX_Projectile.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 ARGX_Projectile::ARGX_Projectile()
@@ -9,6 +10,7 @@ ARGX_Projectile::ARGX_Projectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileMesh"));
 }
 
 // Called when the game starts or when spawned
@@ -24,4 +26,15 @@ void ARGX_Projectile::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+void ARGX_Projectile::SetGenericTeamId(const FGenericTeamId& TeamID)
+{
+	ProjectileTeam = TeamID;
+}
+
+FGenericTeamId ARGX_Projectile::GetGenericTeamId() const
+{
+	return ProjectileTeam;
+}
+
 
--- Source/RegicideX/Actors/Projectiles/RGX_Projectile.h
+++ Source/RegicideX/Actors/Projectiles/RGX_Projectile.h
@@ -4,18 +4,27 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GenericTeamAgentInterface.h"
 #include "RGX_Projectile.generated.h"
 
 class UGameplayEffect;
+class UStaticMeshComponent;
 
 UCLASS()
-class REGICIDEX_API ARGX_Projectile : public AActor
+class REGICIDEX_API ARGX_Projectile : public AActor, public IGenericTeamAgentInterface
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
 	ARGX_Projectile();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* ProjectileMesh = nullptr;
+
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGenericTeamId ProjectileTeam;
 
 protected:
 	// Called when the game starts or when spawned
@@ -25,11 +34,25 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+public:
+	/** Assigns Team Agent to given TeamID */
+	virtual void SetGenericTeamId(const FGenericTeamId& TeamID) override;
+
+	/** Retrieve team identifier in form of FGenericTeamId */
+	virtual FGenericTeamId GetGenericTeamId() const override;
+
+public:
+	/* Actor who fired the bullet */
+	AActor* Instigator = nullptr;
+
 protected:
 	/* Effects the projectile applies when hitting a target */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TArray<TSubclassOf<UGameplayEffect>> EffectToApply;
 
-	/* Actor who fired the bullet */
-	AActor* Instigator = nullptr;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float BaseSpeed = 500.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float LifeSpan = 10.0f;
 };
