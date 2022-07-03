d19e54089e025b2fd49b51f7d9d66a6f2bae0dec
= <alvar.alonso.2010@gmail.com>
2022-07-03 16:28:27 +0200

    RGX_HitboxProjectile added to project

---
+++ Source/RegicideX/Actors/Projectiles/RGX_HitboxProjectile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RegicideX/Actors/Projectiles/RGX_Projectile.h"
+#include "RGX_HitboxProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REGICIDEX_API ARGX_HitboxProjectile : public ARGX_Projectile
+{
+	GENERATED_BODY()
+	
+};
