4bedb8fc592773858f0a196b07572bd6494b281e
= <alvar.alonso.2010@gmail.com>
2022-07-03 15:48:29 +0200

    RGX_GuidedProjectile added to project

---
+++ Source/RegicideX/Actors/Projectiles/RGX_GuidedProjectile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RegicideX/Actors/Projectiles/RGX_Projectile.h"
+#include "RGX_GuidedProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REGICIDEX_API ARGX_GuidedProjectile : public ARGX_Projectile
+{
+	GENERATED_BODY()
+	
+};
