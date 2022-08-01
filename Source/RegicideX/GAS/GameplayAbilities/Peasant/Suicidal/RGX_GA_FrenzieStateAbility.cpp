6d47f6c5082afdf76934c465be7c1ac452be3da9
= <alvar.alonso.2010@gmail.com>
2022-08-01 13:48:30 +0200

    Suicidal FrenzieState ability created

---
+++ Source/RegicideX/GAS/GameplayAbilities/Peasant/Suicidal/RGX_GA_FrenzieStateAbility.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/GameplayAbilities/Peasant/Suicidal/RGX_GA_FrenzieStateAbility.h"
+
---
+++ Source/RegicideX/GAS/GameplayAbilities/Peasant/Suicidal/RGX_GA_FrenzieStateAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/GameplayAbilities/BaseAbilities/RGX_GA_CastSkillAbility.h"
+#include "RGX_GA_FrenzieStateAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REGICIDEX_API URGX_GA_FrenzieStateAbility : public URGX_CastSkillAbility
+{
+	GENERATED_BODY()
+	
+};
