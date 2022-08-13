f1a5af93e4c13bd83b0849c6c5b98d6dd4eda1a1
PabloAcedo <pabloa22.98@gmail.com>
2022-08-13 21:44:59 +0200

    Added shield react

---
+++ Source/RegicideX/Actors/Enemies/RGX_Peasant_Shield.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RGX_Peasant_Shield.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+ARGX_Peasant_Shield::ARGX_Peasant_Shield() 
+{
+
+}
+
+
+void ARGX_Peasant_Shield::HandleDamage(float DamageAmount, const FHitResult& HitInfo, const FGameplayTagContainer& DamageTags, ARGX_CharacterBase* InstigatorCharacter, AActor* DamageCauser)
+{
+	const FVector StartPos = GetActorLocation();
+	const FVector EndPos = StartPos + GetActorForwardVector() * 2000.0f;
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesArray;
+	ObjectTypesArray.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_GameTraceChannel1));
+	TArray<AActor*> ActorsToIgnore;
+	FHitResult OutHit;
+	bool Hitted = UKismetSystemLibrary::SphereTraceSingleForObjects(
+		GetWorld(),
+		StartPos,
+		EndPos,
+		100.0f,
+		ObjectTypesArray,
+		false,
+		ActorsToIgnore,
+		EDrawDebugTrace::None,
+		OutHit,
+		true);
+
+	if (ShieldAmount > 0.0f && Hitted && OutHit.Actor == TargetActor)
+	{
+		ShieldAmount -= 50.0f;
+		PlayAnimMontage(AMShieldBlock);
+	}
+	else 
+	{
+		Super::HandleDamage(DamageAmount, HitInfo, DamageTags, InstigatorCharacter, DamageCauser);
+	}
+}
