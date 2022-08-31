7475bffecd8e48185edface8b8d87a383c95c2ff
= <alvar.alonso.2010@gmail.com>
2022-09-01 01:22:15 +0200

    Blackboard key for target actor in orbit task.

--- Source/RegicideX/AI/Tasks/RGX_BTTask_OrbitAroundTarget.h
+++ Source/RegicideX/AI/Tasks/RGX_BTTask_OrbitAroundTarget.h
@@ -15,6 +15,10 @@ class REGICIDEX_API URGX_BTTask_OrbitAroundTarget : public UBTTaskNode
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FBlackboardKeySelector TargetCharacter;
+
+public:
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 
 	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
