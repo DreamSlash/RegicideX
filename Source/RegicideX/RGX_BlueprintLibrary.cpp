7a2b2c7507ce8fd402861e28769ef54f4fa803b8
= <alvar.alonso.2010@gmail.com>
2022-08-31 15:21:44 +0200

    LaunchToPlayer function and notify, using paraboloid trajectory

--- Source/RegicideX/RGX_BlueprintLibrary.cpp
+++ Source/RegicideX/RGX_BlueprintLibrary.cpp
@@ -3,6 +3,7 @@
 
 #include "RGX_BlueprintLibrary.h"
 #include "RegicideX/GAS/RGX_GameplayEffectContext.h"
+#include "PhysicsEngine/PhysicsSettings.h"
 
 void URGX_BlueprintLibrary::ApplyGameplayEffectContextContainerToActor(const ARGX_CharacterBase* CharacterBase, TSubclassOf<UGameplayEffect> EffectToApply, URGX_RGXEventDataAsset* Payload)
 {
@@ -20,4 +21,32 @@ void URGX_BlueprintLibrary::ApplyGameplayEffectContextContainerToActor(const ARG
 		FGameplayEffectSpec* GESpec = GameplayEffectSpecHandle.Data.Get();
 		ACS->ApplyGameplayEffectSpecToSelf(*GESpec);
 	}
+}
+
+void URGX_BlueprintLibrary::LaunchCharacterToLocation(ARGX_CharacterBase* CharacterBase, const FVector& TargetLocation)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Launch Character To Location. Default Gravity: %f"), UPhysicsSettings::Get()->DefaultGravityZ);
+
+	if (CharacterBase == nullptr) return;
+
+	const FVector CurrentLocation = CharacterBase->GetActorLocation();
+
+	const float HorizontalDistance = FVector::Distance(FVector(CurrentLocation.X, CurrentLocation.Y, 0.0f), FVector(TargetLocation.X, TargetLocation.Y, 0.0f));
+	const float Gravity = 3.0f * UPhysicsSettings::Get()->DefaultGravityZ;
+	const float LaunchAngle = 45.0f;
+	const float Sin = FMath::Sin(LaunchAngle * 2.0f);
+
+	const float Velocity = FMath::Sqrt((HorizontalDistance * FMath::Abs(Gravity)) / Sin);
+	
+	const float xVelocity = Velocity * FMath::Sin(LaunchAngle);
+	const float zVelocity = Velocity * FMath::Cos(LaunchAngle);
+
+	FVector Direction = TargetLocation - CurrentLocation;
+	Direction.Z = 0.0f;
+	Direction.Normalize();
+	const FVector HorizontalLaunchForce = Direction * xVelocity;
+
+	const FVector LaunchVelocity = FVector(HorizontalLaunchForce.X, HorizontalLaunchForce.Y, zVelocity);
+
+	CharacterBase->LaunchCharacter(LaunchVelocity, true, true);
 }
