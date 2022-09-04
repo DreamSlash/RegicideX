85277a3ea5b8abe2e1484f895b4ba19f5f10c458
Sergio5516 <sergio5516@gmail.com>
2022-09-04 22:13:57 +0200

    Fix compilation

--- Source/RegicideX/AI/Tasks/RGX_BTTask_MageAngel_ChooseNextAttack.cpp
+++ Source/RegicideX/AI/Tasks/RGX_BTTask_MageAngel_ChooseNextAttack.cpp
@@ -6,7 +6,7 @@
 
 #include "RegicideX/Actors/Enemies/RGX_EnemyBase.h"
 
-#pragma optimize("", off)
+//#pragma optimize("", off)
 
 EBTNodeResult::Type URGX_BTTask_MageAngel_ChooseNextAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
