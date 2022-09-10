2c9a98dd60ba70a1023781e2e3f69a77831e1627
= <alvar.alonso.2010@gmail.com>
2022-09-10 17:18:38 +0200

    Launch To Player notify gives the option to use forward vector

--- Source/RegicideX/AI/Decorators/RGX_BTDecorator_ActorConeCheck.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "RegicideX/AI/Decorators/RGX_BTDecorator_ActorConeCheck.h"
-
-#include "BehaviorTree/Decorators/BTDecorator_ConeCheck.h"
-
