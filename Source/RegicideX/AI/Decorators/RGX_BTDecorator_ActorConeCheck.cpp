5fcdd7124e79826e7378d35430ddf8532d4dae75
= <alvar.alonso.2010@gmail.com>
2022-09-10 12:02:11 +0200

    decorator created.

---
+++ Source/RegicideX/AI/Decorators/RGX_BTDecorator_ActorConeCheck.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RegicideX/AI/Decorators/RGX_BTDecorator_ActorConeCheck.h"
+
+#include "BehaviorTree/Decorators/BTDecorator_ConeCheck.h"
+
