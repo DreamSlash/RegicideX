549cde7d0d9994c3bd9ff6994fbd900002f816ef
pauflorenti <pauf.97@gmail.com>
2022-10-27 10:36:05 +0200

    Healing power added

--- Source/RegicideX.Target.cs
+++ Source/RegicideX.Target.cs
@@ -10,5 +10,7 @@ public class RegicideXTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.Add("RegicideX");
+		bUseLoggingInShipping = true;
+		bOverrideBuildEnvironment = true;
 	}
 }
