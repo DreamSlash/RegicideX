6ef439de7ef34501ff0af817205e2fdc257c241d
pauflorenti <pauf.97@gmail.com>
2022-11-04 10:46:30 +0100

    Restoring reverted binaries

--- Source/RegicideX/Actors/Enemies/RGX_Peasant_Shield.cpp
+++ Source/RegicideX/Actors/Enemies/RGX_Peasant_Shield.cpp
@@ -6,6 +6,7 @@
 #include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
+#include "RegicideX/Character/RGX_PlayerCharacter.h"
 
 ARGX_Peasant_Shield::ARGX_Peasant_Shield() 
 {
@@ -55,15 +56,7 @@ float ARGX_Peasant_Shield::HandleDamageMitigation(float DamageAmount, const FHit
 		return DamageAmount;
 	}
 
-	/*
-	const FVector StartPos = GetActorLocation() + GetActorForwardVector() * 200.0f;
-	const FVector EndPos = StartPos + GetActorForwardVector() * 2000.0f;
-	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesArray;
-	ObjectTypesArray.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_GameTraceChannel1));
-	TArray<AActor*> ActorsToIgnore;
-	FHitResult OutHit;
-	bool Hitted = UKismetSystemLibrary::SphereTraceSingleForObjects(GetWorld(), StartPos, EndPos, 100.0f, ObjectTypesArray, false, ActorsToIgnore, EDrawDebugTrace::None, OutHit, true);
-	*/
+	ARGX_PlayerCharacter* Player = Cast<ARGX_PlayerCharacter>(InstigatorCharacter);
 
 	FVector MyForward = GetActorForwardVector();
 	MyForward.Z = 0.0f;
@@ -79,12 +72,19 @@ float ARGX_Peasant_Shield::HandleDamageMitigation(float DamageAmount, const FHit
 
 	if (DotProduct > 0.5f)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Shield mitigated damage"));
-		ShieldAmount -= 50.0f;
+		PlayAnimMontage(AMShieldBlock);
+		// If attack is a HeavyAttack, shield takes damage.
+		if (Player && Player->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("Ability.Player.HeavyAttack"))))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Shield is damaged! Attack was heavy."));
+			ShieldAmount -= 50.0f;
+			return 0.0f;
+		}
+
+		UE_LOG(LogTemp, Warning, TEXT("Shield mitigated damage. Shield health: %d"), ShieldAmount);
 		AAIController* AICont = Cast<AAIController>(this->GetController());
 		UBlackboardComponent* BB = AICont->GetBlackboardComponent();
 		BB->SetValueAsBool("bWasHit", true);
-		PlayAnimMontage(AMShieldBlock);
 		return 0.0f;
 	}
 	else
